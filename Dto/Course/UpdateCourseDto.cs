32b70cf88b686c6c8bf032fee9a6a4c05a43035c
lhminhtuan2000 <lhminhtuan2000@gmail.com>
2023-01-04 14:52:42 +0700

    Add course Dto

---
+++ Dto/Course/UpdateCourseDto.cs
@@ -0,0 +1,13 @@
+namespace backend.Dto.Course
+{
+    public class UpdateCourseDto
+    {        
+        public string Name { get; set; } = null!;
+        public bool Online { get; set; }
+        public int Duration { get; set; }
+        public string LearningObjective { get; set; } = null!;
+        public string ImgLink { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public int? TrainerId { get; set; }
+    }
+}
